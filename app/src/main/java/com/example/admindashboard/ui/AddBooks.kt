ce129aa6ffd2577cb745944b5a792ea6ba01c9a9
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-02-15 23:56:42 +0530

    home ui added

---
+++ app/src/main/java/com/example/admindashboard/ui/AddBooks.kt
@@ -0,0 +1,12 @@
+package com.example.admindashboard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.admindashboard.R
+
+class AddBooks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_books)
+    }
+}
