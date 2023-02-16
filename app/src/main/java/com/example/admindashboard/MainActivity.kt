584c795df05217c3b3f7b43ee991f9da2cb55747
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-02-16 09:38:50 +0530

    check box added

--- app/src/main/java/com/example/admindashboard/MainActivity.kt
+++ app/src/main/java/com/example/admindashboard/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.admindashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.admindashboard.databinding.ActivityAddBooksBinding
+import com.example.admindashboard.databinding.ActivityMainBinding
+import com.example.admindashboard.ui.AddBooks
 
 class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.books.setOnClickListener {
+            startActivity(Intent(this, AddBooks::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/admindashboard/ui/AddBooks.kt
+++ app/src/main/java/com/example/admindashboard/ui/AddBooks.kt
@@ -2,11 +2,63 @@ package com.example.admindashboard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.admindashboard.R
+import com.example.admindashboard.databinding.ActivityAddBooksBinding
 
 class AddBooks : AppCompatActivity() {
+    var binding: ActivityAddBooksBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_books)
+        binding = ActivityAddBooksBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        if (binding!!.fe.isChecked) {
+            binding!!.one.visibility = View.VISIBLE
+            binding!!.two.visibility = View.VISIBLE
+
+            binding!!.three.visibility = View.GONE
+            binding!!.four.visibility = View.GONE
+            binding!!.five.visibility = View.GONE
+            binding!!.six.visibility = View.GONE
+            binding!!.seven.visibility = View.GONE
+            binding!!.eight.visibility = View.GONE
+        }
+
+        else if (binding!!.se.isChecked) {
+            binding!!.one.visibility = View.GONE
+            binding!!.two.visibility = View.GONE
+
+            binding!!.three.visibility = View.VISIBLE
+            binding!!.four.visibility = View.VISIBLE
+            binding!!.five.visibility = View.GONE
+            binding!!.six.visibility = View.GONE
+            binding!!.seven.visibility = View.GONE
+            binding!!.eight.visibility = View.GONE
+        }
+
+        else if (binding!!.te.isChecked) {
+            binding!!.one.visibility = View.GONE
+            binding!!.two.visibility = View.GONE
+
+            binding!!.three.visibility = View.GONE
+            binding!!.four.visibility = View.GONE
+            binding!!.five.visibility = View.VISIBLE
+            binding!!.six.visibility = View.VISIBLE
+            binding!!.seven.visibility = View.GONE
+            binding!!.eight.visibility = View.GONE
+        }
+
+        else if (binding!!.te.isChecked) {
+            binding!!.one.visibility = View.GONE
+            binding!!.two.visibility = View.GONE
+
+            binding!!.three.visibility = View.GONE
+            binding!!.four.visibility = View.GONE
+            binding!!.five.visibility = View.GONE
+            binding!!.six.visibility = View.GONE
+            binding!!.seven.visibility = View.VISIBLE
+            binding!!.eight.visibility = View.VISIBLE
+        }
     }
 }
