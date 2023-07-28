d568f57ba71c79563634921965f04b813e95b100
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-07-28 11:06:13 +0530

    rive animation added

--- app/src/main/java/com/example/admindashboard/account/LoginActivity.kt
+++ app/src/main/java/com/example/admindashboard/account/LoginActivity.kt
@@ -3,6 +3,9 @@ package com.example.admindashboard.account
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import com.example.admindashboard.MainActivity
 import com.example.admindashboard.databinding.ActivityLoginBinding
@@ -14,15 +17,16 @@ class LoginActivity : AppCompatActivity() {
 
     private var binding: ActivityLoginBinding? = null
     private lateinit var auth: FirebaseAuth
-
+    private val stateMachineName = "State Machine 1"
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
-        if(currentUser != null){
+        if (currentUser != null) {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -30,32 +34,123 @@ class LoginActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
+        onEmailFoucus()
+        onPasswordFoucus()
+        eyePostionChange()
 
+        subscribeOnClickEvents()
+    }
 
+    private fun subscribeOnClickEvents() {
         binding!!.loginBtn.setOnClickListener {
+            binding!!.password.clearFocus()
+
             val email = binding!!.email.text.toString()
             val password = binding!!.password.text.toString()
 
-            println("$email $password")
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                LoginAccount(email, password)
+            } else {
+                Toast.makeText(this, "Enter Id and Password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this@LoginActivity) { task->
-                    if (task.isSuccessful) {
-                        startActivity(Intent(this, MainActivity::class.java))
-                        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
-                    }else {
-                        binding!!.password.text = null
-                        Toast.makeText(this, "Invalid Id or Password", Toast.LENGTH_SHORT).show()
-                    }
+    private fun LoginAccount(email: String, password: String) {
+
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this@LoginActivity) { task ->
+                if (task.isSuccessful) {
+                    Handler(mainLooper).postDelayed({
+                        binding!!.loginCharacter.controller.fireState(stateMachineName, "success")
+                    }, 2000)
+
+                    Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+
+                    binding!!.password.text = null
+                    Handler(mainLooper).postDelayed({
+                        binding!!.loginCharacter.controller.fireState(stateMachineName, "fail")
+                    }, 2000)
+
+                    Toast.makeText(this, "Invalid Id or Password", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+    }
+
+    private fun eyePostionChange() {
+        binding!!.email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                try {
+                    binding!!.loginCharacter.controller.setNumberState(
+                        stateMachineName,
+                        "Look",
+                        2 * p0!!.length.toFloat()
+                    )
+                } catch (_: Exception) {
 
                 }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+
+            }
+
+        })
+    }
+
+    private fun onPasswordFoucus() {
+//        binding!!.email.clearFocus()
+
+        binding!!.password.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding!!.loginCharacter.controller.setBooleanState(
+                    stateMachineName,
+                    "hands_up",
+                    true
+                )
+            } else {
+                binding!!.loginCharacter.controller.setBooleanState(
+                    stateMachineName,
+                    "hands_up",
+                    false
+                )
+            }
+        }
+    }
+
+    private fun onEmailFoucus() {
+        binding!!.email.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding!!.loginCharacter.controller.setBooleanState(
+                    stateMachineName = stateMachineName,
+                    inputName = "Check",
+                    value = true
+                )
+            } else {
+                binding!!.loginCharacter.controller.setBooleanState(
+                    stateMachineName = stateMachineName,
+                    inputName = "Check",
+                    value = false
+                )
+            }
         }
     }
 
     // releasing resources
     override fun onDestroy() {
         super.onDestroy()
-        if (binding!=null) {
+        if (binding != null) {
             binding = null
         }
     }
